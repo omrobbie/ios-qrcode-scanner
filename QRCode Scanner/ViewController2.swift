3c8349756b361ac1996e6b8b3c8528dc47c1c091
omrobbie <putu.roby@yahoo.com>
2020-02-24 11:48:47 +0800

    add new controller to handle scan result

---
+++ QRCode Scanner/ViewController2.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController2.swift
+//  QRCode Scanner
+//
+//  Created by omrobbie on 24/02/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: ScannerViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
